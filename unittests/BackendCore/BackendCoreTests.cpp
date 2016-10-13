686d6e28f7954f612d0f47601bf42443f47948d1
Than McIntosh <thanm@google.com>
2016-10-13 09:32:30 -0400

    WIP

--- unittests/BackendCore/BackendCoreTests.cpp
+++ unittests/BackendCore/BackendCoreTests.cpp
@@ -19,15 +19,48 @@
 #include "go-linemap.h"
 
 #include "backend.h"
+#include "go-llvm.h"
 
 using namespace llvm;
 
 namespace {
 
-TEST(BackendCoreTests, MakeABackend) {
+TEST(BackendCoreTests, MakeBackend) {
   LLVMContext C;
 
   std::unique_ptr<Backend> makeit(go_get_backend(C));
+
+}
+
+TEST(BackendCoreTests, CoreTypes) {
+  LLVMContext C;
+
+  std::unique_ptr<Backend> backend(go_get_backend(C));
+
+  Btype *et = backend->error_type();
+  ASSERT_TRUE(et != NULL);
+  Btype *vt = backend->void_type();
+  ASSERT_TRUE(vt != NULL);
+  ASSERT_TRUE(vt != et);
+  Btype *bt = backend->bool_type();
+  ASSERT_TRUE(bt != NULL);
+
+  std::vector<bool> isuns = {false, true};
+  std::vector<int> ibits = {8, 16, 32, 64, 128};
+  for (auto uns : isuns) {
+    for (auto nbits : ibits) {
+      Btype *it = backend->integer_type(uns, nbits);
+      ASSERT_TRUE(it != NULL);
+      ASSERT_TRUE(it->type()->isIntegerTy());
+    }
+  }
+
+  std::vector<int> fbits = {32, 64, 128};
+  for (auto nbits : fbits) {
+    Btype *ft = backend->float_type(nbits);
+    ASSERT_TRUE(ft != NULL);
+    ASSERT_TRUE(ft->type()->isFloatingPointTy());
+  }
 
 }
 
