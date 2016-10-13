4ab9653725585385a2a90d25d0560538a0cf5fbd
Than McIntosh <thanm@google.com>
2016-10-13 09:32:30 -0400

    Add StructTypes unit test.

---
+++ llvm-gofrontend/go-llvm.h
@@ -0,0 +1,323 @@
+//===-- go-llvm.h - LLVM implementation of gofrontend 'Backend' class -----===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// Defines Llvm_backend and related classes
+//
+//===----------------------------------------------------------------------===//
+
+// Currently these need to be included before backend.h
+#include "go-location.h"
+#include "go-linemap.h"
+
+#include "backend.h"
+
+#include <unordered_map>
+#include <unordered_set>
+
+namespace llvm {
+class Type;
+}
+
+// Btype wraps llvm::Type
+
+class Btype {
+public:
+  explicit Btype(llvm::Type *type) : type_(type) {}
+
+  llvm::Type *type() const { return type_; }
+
+private:
+  Btype() : type_(NULL) {}
+  llvm::Type *type_;
+  friend class Llvm_backend;
+};
+
+class Llvm_backend : public Backend {
+public:
+  Llvm_backend(llvm::LLVMContext &context);
+
+  // Types.
+
+  Btype *error_type() { return error_type_; }
+
+  Btype *void_type();
+
+  Btype *bool_type();
+
+  Btype *integer_type(bool, int);
+
+  Btype *float_type(int);
+
+  Btype *complex_type(int);
+
+  Btype *pointer_type(Btype *);
+
+  Btype *function_type(const Btyped_identifier &,
+                       const std::vector<Btyped_identifier> &,
+                       const std::vector<Btyped_identifier> &, Btype *,
+                       const Location);
+
+  Btype *struct_type(const std::vector<Btyped_identifier> &);
+
+  Btype *array_type(Btype *, Bexpression *);
+
+  Btype *placeholder_pointer_type(const std::string &, Location, bool);
+
+  bool set_placeholder_pointer_type(Btype *, Btype *);
+
+  bool set_placeholder_function_type(Btype *, Btype *);
+
+  Btype *placeholder_struct_type(const std::string &, Location);
+
+  bool set_placeholder_struct_type(Btype *placeholder,
+                                   const std::vector<Btyped_identifier> &);
+
+  Btype *placeholder_array_type(const std::string &, Location);
+
+  bool set_placeholder_array_type(Btype *, Btype *, Bexpression *);
+
+  Btype *named_type(const std::string &, Btype *, Location);
+
+  Btype *circular_pointer_type(Btype *, bool);
+
+  bool is_circular_pointer_type(Btype *);
+
+  int64_t type_size(Btype *);
+
+  int64_t type_alignment(Btype *);
+
+  int64_t type_field_alignment(Btype *);
+
+  int64_t type_field_offset(Btype *, size_t index);
+
+  // Expressions.
+
+  Bexpression *zero_expression(Btype *);
+
+  Bexpression *error_expression();
+
+  Bexpression *nil_pointer_expression();
+
+  Bexpression *var_expression(Bvariable *var, Location);
+
+  Bexpression *indirect_expression(Btype *, Bexpression *expr, bool known_valid,
+                                   Location);
+
+  Bexpression *named_constant_expression(Btype *btype, const std::string &name,
+                                         Bexpression *val, Location);
+
+  Bexpression *integer_constant_expression(Btype *btype, mpz_t val);
+
+  Bexpression *float_constant_expression(Btype *btype, mpfr_t val);
+
+  Bexpression *complex_constant_expression(Btype *btype, mpc_t val);
+
+  Bexpression *string_constant_expression(const std::string &val);
+
+  Bexpression *boolean_constant_expression(bool val);
+
+  Bexpression *real_part_expression(Bexpression *bcomplex, Location);
+
+  Bexpression *imag_part_expression(Bexpression *bcomplex, Location);
+
+  Bexpression *complex_expression(Bexpression *breal, Bexpression *bimag,
+                                  Location);
+
+  Bexpression *convert_expression(Btype *type, Bexpression *expr, Location);
+
+  Bexpression *function_code_expression(Bfunction *, Location);
+
+  Bexpression *address_expression(Bexpression *, Location);
+
+  Bexpression *struct_field_expression(Bexpression *, size_t, Location);
+
+  Bexpression *compound_expression(Bstatement *, Bexpression *, Location);
+
+  Bexpression *conditional_expression(Btype *, Bexpression *, Bexpression *,
+                                      Bexpression *, Location);
+
+  Bexpression *unary_expression(Operator, Bexpression *, Location);
+
+  Bexpression *binary_expression(Operator, Bexpression *, Bexpression *,
+                                 Location);
+
+  Bexpression *
+  constructor_expression(Btype *, const std::vector<Bexpression *> &, Location);
+
+  Bexpression *array_constructor_expression(Btype *,
+                                            const std::vector<unsigned long> &,
+                                            const std::vector<Bexpression *> &,
+                                            Location);
+
+  Bexpression *pointer_offset_expression(Bexpression *base, Bexpression *offset,
+                                         Location);
+
+  Bexpression *array_index_expression(Bexpression *array, Bexpression *index,
+                                      Location);
+
+  Bexpression *call_expression(Bexpression *fn,
+                               const std::vector<Bexpression *> &args,
+                               Bexpression *static_chain, Location);
+
+  Bexpression *stack_allocation_expression(int64_t size, Location);
+
+  // Statements.
+
+  Bstatement *error_statement();
+
+  Bstatement *expression_statement(Bexpression *);
+
+  Bstatement *init_statement(Bvariable *var, Bexpression *init);
+
+  Bstatement *assignment_statement(Bexpression *lhs, Bexpression *rhs,
+                                   Location);
+
+  Bstatement *return_statement(Bfunction *, const std::vector<Bexpression *> &,
+                               Location);
+
+  Bstatement *if_statement(Bexpression *condition, Bblock *then_block,
+                           Bblock *else_block, Location);
+
+  Bstatement *
+  switch_statement(Bfunction *function, Bexpression *value,
+                   const std::vector<std::vector<Bexpression *>> &cases,
+                   const std::vector<Bstatement *> &statements, Location);
+
+  Bstatement *compound_statement(Bstatement *, Bstatement *);
+
+  Bstatement *statement_list(const std::vector<Bstatement *> &);
+
+  Bstatement *exception_handler_statement(Bstatement *bstat,
+                                          Bstatement *except_stmt,
+                                          Bstatement *finally_stmt, Location);
+
+  // Blocks.
+
+  Bblock *block(Bfunction *, Bblock *, const std::vector<Bvariable *> &,
+                Location, Location);
+
+  void block_add_statements(Bblock *, const std::vector<Bstatement *> &);
+
+  Bstatement *block_statement(Bblock *);
+
+  // Variables.
+
+  Bvariable *error_variable();
+
+  Bvariable *global_variable(const std::string &package_name,
+                             const std::string &pkgpath,
+                             const std::string &name, Btype *btype,
+                             bool is_external, bool is_hidden,
+                             bool in_unique_section, Location location);
+
+  void global_variable_set_init(Bvariable *, Bexpression *);
+
+  Bvariable *local_variable(Bfunction *, const std::string &, Btype *, bool,
+                            Location);
+
+  Bvariable *parameter_variable(Bfunction *, const std::string &, Btype *, bool,
+                                Location);
+
+  Bvariable *static_chain_variable(Bfunction *, const std::string &, Btype *,
+                                   Location);
+
+  Bvariable *temporary_variable(Bfunction *, Bblock *, Btype *, Bexpression *,
+                                bool, Location, Bstatement **);
+
+  Bvariable *implicit_variable(const std::string &, Btype *, bool, bool, bool,
+                               int64_t);
+
+  void implicit_variable_set_init(Bvariable *, const std::string &, Btype *,
+                                  bool, bool, bool, Bexpression *);
+
+  Bvariable *implicit_variable_reference(const std::string &, Btype *);
+
+  Bvariable *immutable_struct(const std::string &, bool, bool, Btype *,
+                              Location);
+
+  void immutable_struct_set_init(Bvariable *, const std::string &, bool, bool,
+                                 Btype *, Location, Bexpression *);
+
+  Bvariable *immutable_struct_reference(const std::string &, Btype *, Location);
+
+  // Labels.
+
+  Blabel *label(Bfunction *, const std::string &name, Location);
+
+  Bstatement *label_definition_statement(Blabel *);
+
+  Bstatement *goto_statement(Blabel *, Location);
+
+  Bexpression *label_address(Blabel *, Location);
+
+  // Functions.
+
+  Bfunction *error_function();
+
+  Bfunction *function(Btype *fntype, const std::string &name,
+                      const std::string &asm_name, bool is_visible,
+                      bool is_declaration, bool is_inlinable,
+                      bool disable_split_stack, bool in_unique_section,
+                      Location);
+
+  Bstatement *function_defer_statement(Bfunction *function,
+                                       Bexpression *undefer, Bexpression *defer,
+                                       Location);
+
+  bool function_set_parameters(Bfunction *function,
+                               const std::vector<Bvariable *> &);
+
+  bool function_set_body(Bfunction *function, Bstatement *code_stmt);
+
+  Bfunction *lookup_builtin(const std::string &);
+
+  void write_global_definitions(const std::vector<Btype *> &,
+                                const std::vector<Bexpression *> &,
+                                const std::vector<Bfunction *> &,
+                                const std::vector<Bvariable *> &);
+
+private:
+  // Make an anonymous Btype from an llvm::Type
+  Btype *make_anon_type(llvm::Type *lt);
+
+  // Create a Btype from an llvm::Type, recording the fact that this
+  // is a placeholder type.
+  Btype *make_placeholder_type(llvm::Type *placeholder);
+
+  // Replace the underlying llvm::Type for a given placeholder type once
+  // we've determined what the final type will be.
+  void update_placeholder_underlying_type(Btype *plt, llvm::Type *newtyp);
+
+  // Create an opaque type for use as part of a placeholder type.
+  llvm::Type *make_opaque_type();
+
+#if 0
+private:
+  void
+  define_builtin(built_in_function bcode, const char* name, const char* libname,
+                 tree fntype, bool const_p, bool noreturn_p);
+#endif
+
+private:
+  typedef std::pair<const std::string, llvm::Type *> named_llvm_type;
+  llvm::LLVMContext &context_;
+  std::unique_ptr<llvm::Module> module_;
+  const llvm::DataLayout &datalayout_;
+  std::unordered_map<llvm::Type *, Btype *> anon_typemap_;
+  // std::unordered_map<named_llvm_type, Btype *> named_typemap_;
+  std::unordered_set<Btype *> placeholders_;
+  Btype *complex_float_type_;
+  Btype *complex_double_type_;
+  Btype *error_type_;
+  llvm::Type *llvm_ptr_type_;
+  unsigned address_space_;
+
+  // A mapping of the LLVM built-ins exposed to Go
+  std::map<std::string, Bfunction *> builtin_functions_;
+};
--- unittests/BackendCore/BackendCoreTests.cpp
+++ unittests/BackendCore/BackendCoreTests.cpp
@@ -19,16 +19,109 @@
 #include "go-linemap.h"
 
 #include "backend.h"
+#include "go-llvm.h"
 
 using namespace llvm;
 
 namespace {
 
-TEST(BackendCoreTests, MakeABackend) {
+TEST(BackendCoreTests, MakeBackend) {
   LLVMContext C;
 
   std::unique_ptr<Backend> makeit(go_get_backend(C));
 
+}
+
+TEST(BackendCoreTests, ScalarTypes) {
+  LLVMContext C;
+
+  std::unique_ptr<Backend> backend(go_get_backend(C));
+
+  Btype *et = backend->error_type();
+  ASSERT_TRUE(et != NULL);
+  Btype *vt = backend->void_type();
+  ASSERT_TRUE(vt != NULL);
+  ASSERT_TRUE(vt != et);
+  Btype *bt = backend->bool_type();
+  ASSERT_TRUE(bt != NULL);
+
+  std::vector<bool> isuns = {false, true};
+  std::vector<int> ibits = {8, 16, 32, 64, 128};
+  for (auto uns : isuns) {
+    for (auto nbits : ibits) {
+      Btype *it = backend->integer_type(uns, nbits);
+      ASSERT_TRUE(it != NULL);
+      ASSERT_TRUE(it->type()->isIntegerTy());
+    }
+  }
+
+  std::vector<int> fbits = {32, 64, 128};
+  for (auto nbits : fbits) {
+    Btype *ft = backend->float_type(nbits);
+    ASSERT_TRUE(ft != NULL);
+    ASSERT_TRUE(ft->type()->isFloatingPointTy());
+  }
+}
+
+//
+// Create this struct using backend interfaces:
+//
+//    struct {
+//       bool f1;
+//       float *f2;
+//       uint64_t f3;
+//    }
+
+static Btype *mkBackendThreeFieldStruct(Backend *be)
+{
+  Btype *pfloat = be->pointer_type(be->float_type(32));
+  Btype *u64 = be->integer_type(true, 64);
+  std::vector<Backend::Btyped_identifier> fields = {
+    Backend::Btyped_identifier("f1", be->bool_type(), Location()),
+    Backend::Btyped_identifier("f2", pfloat, Location()),
+    Backend::Btyped_identifier("f3", u64, Location())
+  };
+  return be->struct_type(fields);
+}
+
+//
+// Create this struct using LLVM interfaces:
+//
+//    struct {
+//       bool f1;
+//       float *f2;
+//       uint64_t f3;
+//    }
+
+static StructType *mkLlvmThreeFieldStruct(LLVMContext &context)
+{
+  SmallVector<Type *, 3> smv(3);
+  smv[0] = Type::getInt1Ty(context);
+  smv[1] = PointerType::get(Type::getFloatTy(context), 0);
+  smv[2] = IntegerType::get(context, 64);
+  return StructType::get(context, smv);
+}
+
+TEST(BackendCoreTests, StructTypes) {
+  LLVMContext C;
+
+  std::unique_ptr<Backend> be(go_get_backend(C));
+
+  // Empty struct
+  std::vector<Backend::Btyped_identifier> nofields;
+  Btype *emptyst = be->struct_type(nofields);
+  SmallVector<Type *, 3> smv_empty(0);
+  Type *llvm_emptyst = StructType::get(C, smv_empty);
+  ASSERT_TRUE(llvm_emptyst != NULL);
+  ASSERT_TRUE(emptyst != NULL);
+  ASSERT_EQ(llvm_emptyst, emptyst->type());
+
+  // Three-field struct
+  Btype *best = mkBackendThreeFieldStruct(be.get());
+  Type *llst = mkLlvmThreeFieldStruct(C);
+  ASSERT_TRUE(best != NULL);
+  ASSERT_TRUE(llst != NULL);
+  ASSERT_EQ(llst, best->type());
 }
 
 }
